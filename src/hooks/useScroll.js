c405ff7255a1f410aa4fcfdc924953eaa6e248b4
Joe Anderson <joe@joes-air.lan>
2019-12-16 21:09:17 -0800

    quick fix

--- src/hooks/useScroll.js
+++ src/hooks/useScroll.js
@@ -1,7 +1,10 @@
 import { useEffect, useState } from "react"
 
 function useScroll() {
-  const [scroll, setScroll] = useState({ x: window.scrollX, y: window.scrollY })
+  const [scroll, setScroll] = useState({
+    x: window ? window.scrollX : 0,
+    y: window ? window.scrollY : 0,
+  })
   function getScroll(event) {
     setScroll({ x: window.scrollX, y: window.scrollY })
   }
