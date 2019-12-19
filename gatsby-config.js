5009c5562d29f0bef362ececfd0da3583ef332a1
Joe Anderson <joe@joes-air.lan>
2019-12-18 20:39:47 -0800

    updated favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,7 @@ module.exports = {
         short_name: `rl`,
         start_url: `/`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
